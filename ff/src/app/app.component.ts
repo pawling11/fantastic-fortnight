7809e87e140f9f0b5aae2ca7d55f10e8bc85baec
isaac.pawling <pawling11@gmail.com>
2018-07-07 09:31:25 -0400

    Finished Angular heroes tut. up through step 3

--- ff/src/app/app.component.ts
+++ ff/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is actually my first Angular App!';
+  title = 'Tour of Heroes - Fantastic Fortnight Version!';
 }
