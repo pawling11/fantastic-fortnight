aea7e2b8092b0abc109a47084e24f2e896bef715
isaac.pawling <pawling11@gmail.com>
2018-07-07 16:31:46 -0400

    Completed Angular tutorial up through pt. 6 (Services)

--- ff/src/app/heroes/heroes.component.ts
+++ ff/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,13 +9,19 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-	id: 1,
-	name: 'Windstorm'
-  };
+  heroes: Hero[];
+  selectedHero: Hero; 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
+  onSelect(hero: Hero): void {
+	this.selectedHero = hero;
+  }
+  getHeroes(): void {
+	this.heroService.getHeroes()
+		.subscribe(heroes => this.heroes = heroes);
+  }
   ngOnInit() {
+	this.getHeroes(); 
   }
 }
